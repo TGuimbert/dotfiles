714b3cc1bb5655d9cfdf38123ab763fac84c41f0
TGuimbert <33598842+TGuimbert@users.noreply.github.com>
2023-03-17 21:51:42 +0100

    Add impermanence & home-manager as Nixos module for Griffin

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     devenv.url = "github:cachix/devenv";
+    impermanence.url = "github:nix-community/impermanence";
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -28,18 +29,38 @@
         griffin = lib.nixosSystem {
           inherit system;
 
-          modules = [ ./modules/nixos ] ++
-            [
-              ./modules/systems/griffin/nixos
-              inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t490
-            ];
+          modules = [
+            home-manager.nixosModules.home-manager
+            inputs.impermanence.nixosModules.impermanence
+            ./modules/nixos
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.tguimbert.imports = [
+                inputs.impermanence.nixosModules.home-manager.impermanence
+                ./modules/home
+              ];
+            }
+          ] ++
+          [
+            ./modules/systems/griffin/nixos
+            {
+              home-manager.users.tguimbert = import ./modules/systems/griffin/home;
+            }
+            inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t490
+          ];
         };
       };
 
       homeConfigurations."tguimbert@griffin" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
-        modules = [ ./modules/home ] ++ [ ./modules/systems/griffin/home ];
+        modules = [
+          ./modules/home
+        ] ++
+        [
+          ./modules/systems/griffin/home
+        ];
       };
 
       devShells.${system}.default = inputs.devenv.lib.mkShell {
