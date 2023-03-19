1a53f36bb47e44cd14503d87576a65e18450d848
TGuimbert <33598842+TGuimbert@users.noreply.github.com>
2023-03-19 17:33:52 +0100

    Add GPG public key

--- modules/home/gpg.nix
+++ modules/home/gpg.nix
@@ -1,10 +1,23 @@
 { ... }:
-
+let
+  publicKey = builtins.fetchurl {
+    url = "https://github.com/TGuimbert.gpg";
+    sha256 = "0c63m4w4z6jsn1glsq5hlf18rl71zj4y4qa7z717w6rf6yx7b9xs";
+  };
+in
 {
+  programs.gpg = {
+    enable = true;
+    publicKeys = [
+      {
+        source = publicKey;
+        trust = 5;
+      }
+    ];
+  };
+
   services.gpg-agent = {
     enable = true;
     pinentryFlavor = "gnome3";
   };
-
-  programs.gpg.enable = true;
 }
