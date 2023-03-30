5e56575d2d626d74d27a581a7812ed0f0c4c9bc9
TGuimbert <33598842+TGuimbert@users.noreply.github.com>
2023-03-30 10:58:24 +0200

    Add Firefox as default browser

--- modules/home/user.nix
+++ modules/home/user.nix
@@ -48,4 +48,31 @@ in
       nix-direnv.enable = true;
     };
   };
+
+  xdg.mimeApps = {
+    defaultApplications = {
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/chrome" = "firefox.desktop";
+      "text/html" = "firefox.desktop";
+      "application/x-extension-htm" = "firefox.desktop";
+      "application/x-extension-html" = "firefox.desktop";
+      "application/x-extension-shtml" = "firefox.desktop";
+      "application/xhtml+xml" = "firefox.desktop";
+      "application/x-extension-xhtml" = "firefox.desktop";
+      "application/x-extension-xht" = "firefox.desktop";
+    };
+    associations.added = {
+      "x-scheme-handler/http" = "firefox.desktop;";
+      "x-scheme-handler/https" = "firefox.desktop;";
+      "x-scheme-handler/chrome" = "firefox.desktop;";
+      "text/html" = "firefox.desktop;";
+      "application/x-extension-htm" = "firefox.desktop;";
+      "application/x-extension-html" = "firefox.desktop;";
+      "application/x-extension-shtml" = "firefox.desktop;";
+      "application/xhtml+xml" = "firefox.desktop;";
+      "application/x-extension-xhtml" = "firefox.desktop;";
+      "application/x-extension-xht" = "firefox.desktop;";
+    };
+  };
 }
