10a986ee902d23948c3b1b83458ae279467c681d
TGuimbert <33598842+TGuimbert@users.noreply.github.com>
2023-08-14 18:15:40 +0200

    Lock lanzaboote version

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     devenv.url = "github:cachix/devenv";
     impermanence.url = "github:nix-community/impermanence";
     lanzaboote = {
-      url = "github:nix-community/lanzaboote";
+      url = "github:nix-community/lanzaboote/v0.3.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
