f851f409f9dc03bbd60fe6dc4e78369dcf594913
TGuimbert <33598842+TGuimbert@users.noreply.github.com>
2024-06-07 17:37:53 +0200

    remove devenv cache server

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -11,13 +11,11 @@
         "https://cache.nixos.org/"
         "https://tguimbert.cachix.org"
         "https://nix-community.cachix.org"
-        "https://devenv.cachix.org"
         "https://nixpkgs-python.cachix.org"
       ];
       trusted-public-keys = [
         "tguimbert.cachix.org-1:PDa22nLjEwxsABhCz09ONTfYAP3DJOAJRszoy007ojs="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
         "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
       ];
       auto-optimise-store = true;
