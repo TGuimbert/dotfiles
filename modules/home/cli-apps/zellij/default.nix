be9159454e1cb9313d82057e6d4ba0c483f5d229
TGuimbert <33598842+TGuimbert@users.noreply.github.com>
2024-09-24 23:14:54 +0200

    feat: remove zellij helix collisions

--- modules/home/cli-apps/zellij/default.nix
+++ modules/home/cli-apps/zellij/default.nix
@@ -4,18 +4,57 @@
     zellij = {
       enable = true;
       enableFishIntegration = true;
-      settings = {
-        default_shell = "fish";
-        ui = {
-          pane_frames = {
-            rounded_corners = true;
-          };
-        };
-      };
     };
   };
   home.sessionVariables = {
     ZELLIJ_AUTO_ATTACH = "true";
     ZELLIJ_AUTO_EXIT = "true";
+  };
+  home.file.".config/zellij/config.kdl" = {
+    text = ''
+      default_shell "fish"
+      ui {
+        pane_frames {
+          rounded_corners true
+        }
+      }
+
+      // Remove collisions with Helix keybindings
+      keybinds {
+        scroll {
+          unbind "Ctrl s"
+          bind "Ctrl y" { SwitchToMode "Normal"; }
+        }
+        search {
+          unbind "Ctrl s"
+          bind "Ctrl y" { SwitchToMode "Normal"; }
+        }
+        session {
+          unbind "Ctrl o"
+          bind "Ctrl e" { SwitchToMode "Normal"; }
+          unbind "Ctrl s"
+          bind "Ctrl y" { SwitchToMode "Normal"; }
+        }
+        shared_except "locked" {
+          unbind "Alt i"
+          bind "Alt w" { MoveTab "Left"; }
+          unbind "Alt o"
+          bind "Alt q" { MoveTab "Right"; }
+          unbind "Alt n"
+          bind "Alt m" { NewTab; }
+        }
+        shared_except "scroll" "locked" {
+          unbind "Ctrl s"
+          bind "Ctrl y" { SwitchToMode "Scroll"; }
+        }
+        shared_except "session" "locked" {
+          unbind "Ctrl o"
+          bind "Ctrl e" { SwitchToMode "Session"; }
+        }
+        shared_except "tmux" "locked" {
+          unbind "Ctrl b"
+        }
+      }
+    '';
   };
 }
