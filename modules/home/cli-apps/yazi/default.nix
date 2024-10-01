0ab410b1989670633ef200c44d7ceb63a3de0008
TGuimbert <33598842+TGuimbert@users.noreply.github.com>
2024-10-01 22:31:33 +0200

    feat: add yazi

---
+++ modules/home/cli-apps/yazi/default.nix
@@ -0,0 +1,70 @@
+{ ... }:
+{
+  programs.yazi = {
+    enable = true;
+    enableFishIntegration = true;
+    settings = {
+      manager = {
+        ratio = [
+          0
+          4
+          0
+        ];
+      };
+      opener = {
+        edit = [
+          {
+            run = "~/.config/yazelix/yazi/open_file.sh $1";
+            desc = "Open File in a new pane";
+          }
+        ];
+      };
+    };
+
+  };
+  # init.lua will be a proper option after 24.05
+  xdg.configFile = {
+    "yazi/init.lua".text = ''
+      function Status:render(area)
+      	self.area = area
+
+      	local line = ui.Line { self:percentage(), self:position() }
+      	return {
+      		ui.Paragraph(area, { line }):align(ui.Paragraph.CENTER),
+      	}
+      end
+    '';
+    "yazi/open_file.sh" = {
+      executable = true;
+      text = ''
+        #!/usr/bin/env sh
+
+        # Move focus to the next pane
+        zellij action focus-next-pane
+
+        # Get the running command in the current pane
+        RUNNING_COMMAND=$(zellij action list-clients | awk 'NR==2 {print $3}')
+
+        # Check if the command running in the current pane is helix (hx)
+        if echo "$RUNNING_COMMAND" | grep -q "/hx$"; then
+            # The current pane is running helix, use zellij actions to open the file
+            zellij action write 27
+            zellij action write-chars ":open $1"
+            zellij action write 13
+        else
+            # The current pane is not running helix, so open helix in a new pane
+            zellij action new-pane
+            sleep 0.4
+            # Get the working directory
+            if [ -d "$1" ]; then
+                WORKING_DIR="$1"
+            else
+                WORKING_DIR=$(dirname "$1")
+            fi
+            zellij action write-chars "hx $1 -w $WORKING_DIR"
+            zellij action write 13
+        fi
+      '';
+    };
+  };
+}
--- modules/home/cli-apps/zellij/default.nix
+++ modules/home/cli-apps/zellij/default.nix
@@ -4,55 +4,97 @@
     zellij = {
       enable = true;
       enableFishIntegration = true;
+      settings = {
+        default_shell = "fish";
+        ui.pane_frames.rounded_corners = true;
+        keybinds = {
+          scroll = {
+            "unbind \"Ctrl s\"" = { };
+            "bind \"Ctrl y\"" = {
+              SwitchToMode = "Normal";
+            };
+          };
+          search = {
+            "unbind \"Ctrl s\"" = { };
+            "bind \"Ctrl y\"" = {
+              SwitchToMode = "Normal";
+            };
+          };
+          session = {
+            "unbind \"Ctrl o\"" = { };
+            "bind \"Ctrl e\"" = {
+              SwitchToMode = "Normal";
+            };
+            "unbind \"Ctrl s\"" = { };
+            "bind \"Ctrl y\"" = {
+              SwitchToMode = "Scroll";
+            };
+          };
+          "shared_except \"locked\"" = {
+            "unbind \"Alt i\"" = { };
+            "bind \"Alt w\"" = {
+              MoveTab = "Left";
+            };
+            "unbind \"Alt o\"" = { };
+            "bind \"Alt q\"" = {
+              MoveTab = "Right";
+            };
+            "unbind \"Alt n\"" = { };
+            "bind \"Alt m\"" = {
+              NewPane = "";
+            };
+          };
+          "shared_except \"scroll\" \"locked\"" = {
+            "unbind \"Ctrl s\"" = { };
+            "bind \"Ctrl y\"" = {
+              SwitchToMode = "Scroll";
+            };
+          };
+          "shared_except \"session\" \"locked\"" = {
+            "unbind \"Ctrl o\"" = { };
+            "bind \"Ctrl e\"" = {
+              SwitchToMode = "Session";
+            };
+          };
+          "shared_except \"tmux\" \"locked\"" = {
+            "unbind \"Ctrl b\"" = { };
+          };
+        };
+      };
     };
   };
-  home.sessionVariables = {
-    ZELLIJ_AUTO_ATTACH = "true";
-    ZELLIJ_AUTO_EXIT = "true";
+  home = {
+    sessionVariables = {
+      ZELLIJ_AUTO_ATTACH = "true";
+      ZELLIJ_AUTO_EXIT = "true";
+    };
   };
-  home.file.".config/zellij/config.kdl" = {
-    text = ''
-      default_shell "fish"
-      ui {
-        pane_frames {
-          rounded_corners true
+  xdg.configFile = {
+    "zellij/layouts/yazelix.kdl".text = ''
+      layout {
+        tab_template name="ui" {
+          pane size=1 borderless=true {
+            plugin location="zellij:tab-bar"
+          }
+          children
+          pane size=1 borderless=true {
+            plugin location="zellij:status-bar"
+          }
         }
-      }
 
-      // Remove collisions with Helix keybindings
-      keybinds {
-        scroll {
-          unbind "Ctrl s"
-          bind "Ctrl y" { SwitchToMode "Normal"; }
-        }
-        search {
-          unbind "Ctrl s"
-          bind "Ctrl y" { SwitchToMode "Normal"; }
-        }
-        session {
-          unbind "Ctrl o"
-          bind "Ctrl e" { SwitchToMode "Normal"; }
-          unbind "Ctrl s"
-          bind "Ctrl y" { SwitchToMode "Normal"; }
-        }
-        shared_except "locked" {
-          unbind "Alt i"
-          bind "Alt w" { MoveTab "Left"; }
-          unbind "Alt o"
-          bind "Alt q" { MoveTab "Right"; }
-          unbind "Alt n"
-          bind "Alt m" { NewTab; }
-        }
-        shared_except "scroll" "locked" {
-          unbind "Ctrl s"
-          bind "Ctrl y" { SwitchToMode "Scroll"; }
-        }
-        shared_except "session" "locked" {
-          unbind "Ctrl o"
-          bind "Ctrl e" { SwitchToMode "Session"; }
-        }
-        shared_except "tmux" "locked" {
-          unbind "Ctrl b"
+        default_tab_template {
+          pane size=1 borderless=true {
+            plugin location="zellij:tab-bar"
+          }
+          pane split_direction="vertical" {
+            pane name="sidebar" {
+              command "yazi"
+              size "100%"
+            }
+          }
+          pane size=1 borderless=true {
+            plugin location="zellij:status-bar"
+          }
         }
       }
     '';
