4ba15ac37583f568e3fc4b96ed485d87a8ed1bfb
TGuimbert <33598842+TGuimbert@users.noreply.github.com>
2024-11-26 17:04:12 +0100

    improve helix config

--- modules/home/cli-apps/helix/default.nix
+++ modules/home/cli-apps/helix/default.nix
@@ -1,8 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.helix = {
     enable = true;
     defaultEditor = true;
+    extraPackages = with pkgs; [
+      marksman
+      ltex-ls
+      yaml-language-server
+      nodePackages.prettier
+    ];
     ignores = [
       ".obsidian/"
       ".direnv/"
@@ -12,6 +18,10 @@
       language = [
         {
           name = "markdown";
+          language-servers = [
+            "marksman"
+            "ltex-ls"
+          ];
           formatter = {
             command = "dprint";
             args = [
@@ -32,9 +42,44 @@
         {
           name = "go";
           auto-format = true;
-          formatter = { command = "goimports"; };
+          formatter = {
+            command = "goimports";
+          };
+        }
+        {
+          name = "yaml";
+          auto-format = true;
+          formatter = {
+            command = "prettier";
+            args = [
+              "--parser"
+              "yaml"
+            ];
+          };
         }
       ];
+      language-server.yaml-language-server.config.yaml = {
+        completion = true;
+        validation = true;
+        hover = true;
+        schemas = {
+          "https://json.schemastore.org/github-workflow.json" = ".github/workflows/*.{yml,yaml}";
+          "https://raw.githubusercontent.com/ansible-community/schemas/main/f/ansible-tasks.json" = "roles/{tasks,handlers}/*.{yml,yaml}";
+          kubernetes = [
+            "*deployment*.yaml"
+            "*service*.yaml"
+            "*configmap*.yaml"
+            "*secret*.yaml"
+            "*pod*.yaml"
+            "*namespace*.yaml"
+            "*ingress*.yaml"
+          ];
+          "https://raw.githubusercontent.com/SchemaStore/schemastore/master/src/schemas/json/kustomization.json" = [
+            "*kustomization.yaml"
+            "*kustomize.yaml"
+          ];
+        };
+      };
     };
     settings = {
       editor = {
