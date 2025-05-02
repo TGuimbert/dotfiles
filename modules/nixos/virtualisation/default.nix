cca901ab0dc223e6b61a942614ae660591114d64
TGuimbert <33598842+TGuimbert@users.noreply.github.com>
2025-05-02 15:28:30 +0200

    add qemu by default

--- modules/nixos/virtualisation/default.nix
+++ modules/nixos/virtualisation/default.nix
@@ -1,18 +1,29 @@
-{ options, pkgs, lib, ... }:
+{ pkgs
+, lib
+, ...
+}:
 with lib;
 with pkgs.lib.options;
 {
   options.tguimbert.virtualisation = {
-    containerPlatform =
-      mkOption {
-        type = types.nullOr (types.enum [ "podman" "docker" ]);
-        default = null;
-        description =
-          lib.mdDoc ''
-            This option determines which container platform to use. By default
-            it does not install any.
-          '';
-      };
+    containerPlatform = mkOption {
+      type = types.nullOr (
+        types.enum [
+          "podman"
+          "docker"
+        ]
+      );
+      default = null;
+      description = lib.mdDoc ''
+        This option determines which container platform to use. By default
+        it does not install any.
+      '';
+    };
   };
 
+  config = {
+    environment = {
+      systemPackages = [ pkgs.qemu ];
+    };
+  };
 }
