fd44b680dc1ce6e6a2506bc03ef825880c897edc
TGuimbert <33598842+TGuimbert@users.noreply.github.com>
2025-06-02 11:28:08 +0200

    add rust shell

--- shells/rust/default.nix
+++ shells/rust/default.nix
@@ -6,6 +6,15 @@ pkgs.mkShell {
     gcc
     rustfmt
     clippy
+    rust-analyzer
+    lldb
+    pkg-config
+    openssl
+    rustfinity
   ];
   RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+
+  shellHook = ''
+    export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
+  '';
 }
