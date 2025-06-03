17be1afabca02ac6856eb0bbc52257f09e7719b0
TGuimbert <33598842+TGuimbert@users.noreply.github.com>
2025-06-03 21:19:12 +0200

    fix starship theme

--- modules/home/apps/wezterm/default.nix
+++ modules/home/apps/wezterm/default.nix
@@ -20,10 +20,10 @@
       enableTransience = true;
       settings = {
         # Based on the gruvbox-rainbow preset
-        format = "[](color_orange)$os$username[](bg:color_yellow fg:color_orange)$directory[](fg:color_yellow bg:color_aqua)$git_branch$git_status[](fg:color_aqua bg:color_blue)$c$rust$golang$nodejs$php$java$kotlin$haskell$python[](fg:color_blue bg:color_bg3)$docker_context$conda[](fg:color_bg3 bg:color_bg1)$time[ ](fg:color_bg1)$line_break$character";
+        format = "[](orange)$os$username[](bg:bright-yellow fg:orange)$directory[](fg:bright-yellow bg:bright-cyan)$git_branch$git_status[](fg:bright-cyan bg:bright-blue)$c$rust$golang$nodejs$php$java$kotlin$haskell$python[](fg:bright-blue bg:bright-black)$docker_context$conda[](fg:bright-black bg:base01)$time[ ](fg:base01)$line_break$character";
         os = {
           disabled = false;
-          style = "bg:color_orange fg:color_fg0";
+          style = "bg:orange fg:bright-white";
           symbols = {
             Windows = "󰍲";
             Ubuntu = "󰕈";
@@ -49,12 +49,12 @@
         };
         username = {
           show_always = true;
-          style_user = "bg:color_orange fg:color_fg0";
-          style_root = "bg:color_orange fg:color_fg0";
+          style_user = "bg:orange fg:bright-white";
+          style_root = "bg:orange fg:bright-white";
           format = "[ $user ]($style)";
         };
         directory = {
-          style = "fg:color_fg0 bg:color_yellow";
+          style = "fg:bright-white bg:bright-yellow";
           format = "[ $path ]($style)";
           truncation_length = 3;
           truncation_symbol = "…/";
@@ -68,72 +68,72 @@
         };
         git_branch = {
           symbol = "";
-          style = "bg:color_aqua";
-          format = "[[ $symbol $branch ](fg:color_fg0 bg:color_aqua)]($style)";
+          style = "bg:bright-cyan";
+          format = "[[ $symbol $branch ](fg:bright-white bg:bright-cyan)]($style)";
         };
         git_status = {
-          style = "bg:color_aqua";
-          format = "[[($all_status$ahead_behind )](fg:color_fg0 bg:color_aqua)]($style)";
+          style = "bg:bright-cyan";
+          format = "[[($all_status$ahead_behind )](fg:bright-white bg:bright-cyan)]($style)";
         };
         nodejs = {
           symbol = "";
-          style = "bg:color_blue";
-          format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+          style = "bg:bright-blue";
+          format = "[[ $symbol( $version) ](fg:bright-white bg:bright-blue)]($style)";
         };
         c = {
           symbol = " ";
-          style = "bg:color_blue";
-          format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+          style = "bg:bright-blue";
+          format = "[[ $symbol( $version) ](fg:bright-white bg:bright-blue)]($style)";
         };
         rust = {
           symbol = "";
-          style = "bg:color_blue";
-          format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+          style = "bg:bright-blue";
+          format = "[[ $symbol( $version) ](fg:bright-white bg:bright-blue)]($style)";
         };
         golang = {
           symbol = "";
-          style = "bg:color_blue";
-          format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+          style = "bg:bright-blue";
+          format = "[[ $symbol( $version) ](fg:bright-white bg:bright-blue)]($style)";
         };
         php = {
           symbol = "";
-          style = "bg:color_blue";
-          format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+          style = "bg:bright-blue";
+          format = "[[ $symbol( $version) ](fg:bright-white bg:bright-blue)]($style)";
         };
         java = {
           symbol = " ";
-          style = "bg:color_blue";
-          format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+          style = "bg:bright-blue";
+          format = "[[ $symbol( $version) ](fg:bright-white bg:bright-blue)]($style)";
         };
         kotlin = {
           symbol = "";
-          style = "bg:color_blue";
-          format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+          style = "bg:bright-blue";
+          format = "[[ $symbol( $version) ](fg:bright-white bg:bright-blue)]($style)";
         };
         haskell = {
           symbol = "";
-          style = "bg:color_blue";
-          format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+          style = "bg:bright-blue";
+          format = "[[ $symbol( $version) ](fg:bright-white bg:bright-blue)]($style)";
         };
         python = {
           symbol = "";
-          style = "bg:color_blue";
-          format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_blue)]($style)";
+          style = "bg:bright-blue";
+          format = "[[ $symbol( $version) ](fg:bright-white bg:bright-blue)]($style)";
         };
         docker_context = {
           symbol = "";
-          style = "bg:color_bg3";
-          format = "[[ $symbol( $context) ](fg:#83a598 bg:color_bg3)]($style)";
+          style = "bg:bright-black";
+          format = "[[ $symbol( $context) ](fg:#83a598 bg:bright-black)]($style)";
         };
         conda = {
-          style = "bg:color_bg3";
-          format = "[[ $symbol( $environment) ](fg:#83a598 bg:color_bg3)]($style)";
+          style = "bg:bright-black";
+          format = "[[ $symbol( $environment) ](fg:#83a598 bg:bright-black)]($style)";
         };
         time = {
           disabled = false;
           time_format = "%R";
-          style = "bg:color_bg1";
-          format = "[[  $time ](fg:color_fg0 bg:color_bg1)]($style)";
+          style = "bg:base01";
+          format = "[[  $time ](fg:bright-white bg:base01)]($style)";
         };
         line_break = {
           disabled = false;
