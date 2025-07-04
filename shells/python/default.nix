63ee998bdfa963aac65620e0418fb36f2a53cd3e
TGuimbert <33598842+TGuimbert@users.noreply.github.com>
2025-07-04 22:05:47 +0200

    rework python shell

--- shells/python/default.nix
+++ shells/python/default.nix
@@ -1,34 +1,21 @@
 { pkgs, channels, ... }:
 pkgs.mkShell {
-  packages = with pkgs; [
-    python310
-    channels.unstable.uv
+  packages = with channels.unstable; [
+    uv
+    fnm
+    # Language Server Protocol
+    python313Packages.python-lsp-server
     ruff
-    pre-commit
-    python312Packages.python-lsp-server
-    python312Packages.pip
-    # Postgis libraries
-    gdal
-    geos
-    # TOML lsp
-    taplo
-    # Other libraries
-    zlib
-    glib
-    libglvnd
-    file
+    python313Packages.pylsp-mypy
   ];
   shellHook = with pkgs; ''
-    # uv has to use copy mode because hardlinks do not work with impermanence
+    export LD_LIBRARY_PATH="${
+      lib.makeLibraryPath [
+      ]
+    }:$LD_LIBRARY_PATH"
+    export GDAL_LIBRARY_PATH="${lib.makeLibraryPath [ pkgs.gdal ]}/libgdal.so"
+    export "GEOS_LIBRARY_PATH=${lib.makeLibraryPath [ pkgs.geos ]}/libgeos_c.so"
     export UV_LINK_MODE=copy
-    uv venv --link-mode copy --allow-existing --quiet
-    source .venv/bin/activate
-    # Postgis libraries
-    export "GDAL_LIBRARY_PATH=${gdal}/lib/libgdal.so"
-    export "GEOS_LIBRARY_PATH=${geos}/lib/libgeos_c.so"
-    # Other libraries
-    export LD_LIBRARY_PATH="${zlib}/lib:${glib.out}/lib:${libglvnd}/lib:${pkgs.stdenv.cc.cc.lib}/lib:${gfortran.cc.lib}/lib:${file}/lib"
-    # Skip ruff in pre-commit
-    export "SKIP=ruff,ruff-format"
+    eval "$(fnm env)"
   '';
 }
