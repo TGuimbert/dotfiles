503aee8734907648fb52a03586535b80ee36d198
TGuimbert <33598842+TGuimbert@users.noreply.github.com>
2026-02-22 12:35:25 +0100

    fix freecad missing dependencies

--- overlays/default.nix
+++ overlays/default.nix
@@ -18,4 +18,9 @@ final: prev: {
     azure-cli.withExtensions [
       azure-cli.extensions.ssh
     ];
+
+  # Temporary fix to this issue: https://github.com/NixOS/nixpkgs/issues/467783#issuecomment-3621306981
+  freecad = prev.freecad.overrideAttrs (old: {
+    nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ prev.wrapGAppsHook3 ];
+  });
 }
