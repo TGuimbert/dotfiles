d2328eb7e66a1d610dda73ef9c66909d55166a19
Thibault Guimbert <33598842+TGuimbert@users.noreply.github.com>
2026-07-18 16:14:45 +0200

    feat(facter): migrate leshen to nixos-facter hardwaer detection (#295)

--- modules/_hosts/leshen/hardware.nix
+++ modules/_hosts/leshen/hardware.nix
@@ -1,35 +1,11 @@
+{ ... }:
 {
-  config,
-  lib,
-  modulesPath,
-  ...
-}:
-
-{
-  imports = [
-    (modulesPath + "/installer/scan/not-detected.nix")
-  ];
+  hardware.facter.reportPath = ./facter.json;
 
   boot = {
-    initrd = {
-      availableKernelModules = [
-        "nvme"
-        "xhci_pci"
-        "ahci"
-        "usb_storage"
-        "usbhid"
-        "sd_mod"
-      ];
-      kernelModules = [ ];
-    };
-    kernelModules = [ "kvm-amd" ];
-    extraModulePackages = [ ];
-
     supportedFilesystems = [ "zfs" ];
     zfs.forceImportRoot = false;
   };
-
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   networking.hostId = "42dea791";
 }
